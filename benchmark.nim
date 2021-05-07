f939ad00dc0927c71073e5201def76f710e15a19
Juan Carlos <juancarlospaco@gmail.com>
2021-05-06 22:29:39 -0300

    update

--- benchmark.nim
+++ benchmark.nim
@@ -9,57 +9,57 @@ template tableBench(name: string; code) =
     for i in 0 .. sizeForBench: tabla.add $i, false
     let t = now()
     code
-    echo "TABLE\t", name, "()\t", now() - t
+    result.add "  | `" & name & "()`   | " & $(now() - t)
 
-template dikBench(name: string; code) =
+template dikBench(code) =
   block:
     var dict {.inject.} = newDikOfCap[bool](sizeForBench)
     for i in 0 .. sizeForBench: dict.add $i, false
     let t = now()
     code
-    echo "DIK\t", name, "()\t", now() - t
+    result.add " | " & $(now() - t) & " |\n"
 
-template line() = echo "------------------------------------------------------------------------------"
+proc main(): string =
+  result = """
+  | Operation | `Table`          | `Dik`               |
+  |-----------|------------------|---------------------|
+"""
 
-proc main() =
   tableBench "del":
     for i in 0 .. sizeForBench:
       tabla.del $i
 
-  dikBench "del":
+  dikBench:
     for i in 0 .. sizeForBench:
       dict.del $i
 
-  line()
 
   tableBench "clear":
     for i in 0 .. sizeForBench:
       tabla.clear()
 
-  dikBench "clear":
+  dikBench:
     for i in 0 .. sizeForBench:
       dict.clear()
 
-  line()
 
   tableBench "add":
     for i in 0 .. sizeForBench:
       tabla.add $i, false
 
-  dikBench "add":
+  dikBench:
     for i in 0 .. sizeForBench:
       dict.add $i, false
 
-  line()
 
   tableBench "get":
     for i in 0 .. sizeForBench:
       discard tabla[$i]
 
-  dikBench "get":
+  dikBench:
     for i in 0 .. sizeForBench:
       discard dict[$i]
 
 
 when isMainModule:
-  main()
+  echo main()
